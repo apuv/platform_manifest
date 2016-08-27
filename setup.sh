ec998d4fa41940c6d74eb6441a75966763c1d348
Apu V <theapu@gmail.com>
2016-08-28 00:02:40 +0530

    Update setup.sh
    Branch updated

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@ clear
 cd
 mkdir cyanogenmod
 cd cyanogenmod
-repo init -u https://github.com/apuv/platform_manifest.git -b stable/cm-13.0-ZNH2K
+repo init -u https://github.com/apuv/platform_manifest.git -b stable/cm-13.0-ZNH5Y
 repo sync -j 4
